80f820a7420980e7f221fdcd6b0fe697270d0a2d
Batu22 <batuhantemel14@gmail.com>
2023-10-25 01:19:26 +0300

    back to_top register,login modal,dark mode,input search

--- template.js
+++ template.js
@@ -1,1 +1,25 @@
-// alert("UYARI");
+// Dark Mode
+let darkModu = () => {
+    window.document.body.classList.toggle("dark_mode");
+}
+
+// JQUERY
+// input search
+$(document).ready(function () {
+    const searchApi = ["İzmir", "Ankara", "Balikesir", "İstanbul", "Gaziantep", "Malatya", "Elazığ", "Antalya"];
+    $("#tags").autocomplete({
+        source: searchApi,
+    })
+}); // end
+
+//Footer
+let newDate = () => {
+    //DOM
+    // let date = document.querySelector("#date_id");
+    let date = document.getElementById("date_id");
+    date.innerHTML = new Date().getFullYear();
+
+    // Jquery DOM
+    // $("#date_id").html(new Date().getFullYear());
+}
+newDate();
