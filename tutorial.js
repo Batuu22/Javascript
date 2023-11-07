df866cce89961f04543cd9a806edc8cb1636729b
Batu22 <batuhantemel14@gmail.com>
2023-11-07 18:26:58 +0300

    variables,whoisting,alert,documen.writleln,console,win dow

---
+++ tutorial.js
@@ -0,0 +1,18 @@
+// JS 1995 Yılında Brandan Eich tarafından yazılıyor
+// cross-platform = farklı işletim sistemlerinde çalışabiliyor
+// OOP Uygun
+
+// document.write("HELLO WORLD <br/>");
+// document.writeln("MERHABA DUNYA");
+// console.info("Hello world");
+// console.error("warn");
+
+// Variables
+
+//whoisting
+result2=56;
+var result2;
+
+///////////////////////////////////////////////////////////////////////
+// Operators
+
