0d024209cf6fbaae7f8dcf2a820c8ad9154cf354
Batu22 <batuhantemel14@gmail.com>
2023-11-09 17:44:08 +0300

    JS String Methods practice homework

---
+++ homework/homework.js
@@ -0,0 +1,31 @@
+// Ödev 
+// ÖDEV NOT:5 adımı beraber yapalım.
+// kullanıcı tarafından girilen bir kelimenin (prompt)
+// let userData= prompt("Lütfen bir şeyler yazınız");
+// S-1) Kaç karakterlidir ? (length)
+// S-2) boşluklar alınarak Kaç karakterlidir ? (trim)
+// S-3) bütün kelimeyi küçük harfle göstermek ? (toLowerCase())
+// S-4) bütün kelimeyi büyük harfle göstermek ? (toUpperCase())
+// S-5) Kullanıcıdan alınan ilk kelimeyle , "javascript" kelimeyle değiştirin. (replace, substring, indexOf)
+// console.log(userData.replace("xxxxxxx"),"javascript");
+// console.log(userData.replace(userData.substring(0,userData.indexOf(" ")),"javascript"));
+// S-6) yazdığınız kelimede "javascript ile mi başlıyor" ? (startsWith()) 
+// S-7) yazdığınız kelimede "javascript ile mi bitiyor" ? (endsWith())
+// S-8) ilk harf nedir  ? (charAt())
+// S-9) girdiğiniz kelimenin sonuna "-ben js öğreniyorum" (concat())
+// S-10) girdiğiniz 0 ile 4 arasındaki indexi ekranda gösterin? (subString())
+
+
+// çözümü...
+let inpWord=prompt("bir kelime giriniz");
+console.log(inpWord.length);
+console.log(inpWord.trim().length);
+console.log(inpWord.toLowerCase());
+console.log(inpWord.toUpperCase());
+let inpAltWord=inpWord.replace(inpWord.substring(0,inpWord.indexOf(" ")),"javascript")
+console.log(inpAltWord);
+console.log(inpAltWord.startsWith("javascript"));
+console.log(inpAltWord.endsWith("javascript"));
+console.log(inpAltWord.charAt(0));
+console.log(inpWord.concat("ben js öğreniyorum"));
+console.log(inpWord.slice(0,4));
