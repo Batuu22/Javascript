936d019d9c67f368db592c12cc72d223279b53fa
Batu22 <batuhantemel14@gmail.com>
2023-12-01 23:58:03 +0300

    JQuery

--- tutorials.js
+++ tutorials.js
@@ -802,16 +802,16 @@ let paramCallApplyBindTuto = () => {
 //paramCallApplyBindTuto();
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 //event
-let clicking=()=>{
+let clicking = () => {
     window.alert("Clicked");
 }
-let dblClicking=()=>{
+let dblClicking = () => {
     document.writeln("dblClicked");
 }
-let mousing=()=>{
+let mousing = () => {
     alert("Buton üzerine gelindi.");
 }
-let keyDownEvent=()=>{
+let keyDownEvent = () => {
     alert("tuş çalıştı");
 }
 //////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -861,9 +861,9 @@ let keyDownEvent=()=>{
 //////////////////////////////////////////////////////////////////////////////////////////////////////
 //listener
 // Ödev : Kullanıcı bu paragrafa kaç kere tıkladı ?
-let userListener=()=>{
-    let listenerParag=document.getElementById("parag_list");
-    listenerParag.addEventListener("click",function(e){
+let userListener = () => {
+    let listenerParag = document.getElementById("parag_list");
+    listenerParag.addEventListener("click", function (e) {
         //alert(e.preventDefault() + e.target);
         //document.getElementById("log_admin").innerHTML="takip ediliyor";
         $("#log_admin").html("takip ediliyor"); // => Jquery
@@ -875,23 +875,23 @@ let userListener=()=>{
 // veriler bizim için önemlidir.
 // verileri saklarız , => database,browser
 
-let localStorageData=()=>{
-    const id=localStorage.setItem("ID",1);
-    const name=localStorage.setItem("name","Batuhan");
-    const surname=localStorage.setItem("Surname","Temel");
+let localStorageData = () => {
+    const id = localStorage.setItem("ID", 1);
+    const name = localStorage.setItem("name", "Batuhan");
+    const surname = localStorage.setItem("Surname", "Temel");
     //console.log(localStorage);
     //console.log(localStorage.getItem(localStorage.key(0)));
     //console.log(localStorage.length);
-    const surnameData=localStorage.getItem("Surname");
+    const surnameData = localStorage.getItem("Surname");
     console.log(surnameData);
     localStorage.removeItem("ID"); // => localStoragedaki bir datayı silmek istersek.
     console.log(localStorage);
     localStorage.clear(); // => localStorage daki bütün dataları siler
 
-    const localStorageUserName=prompt("İsim giriniz");
-    const localStorageUserSurname=prompt("Soyisim giriniz");
-    localStorage.setItem("Name",localStorageUserName);
-    localStorage.setItem("Surname",localStorageUserSurname);
+    const localStorageUserName = prompt("İsim giriniz");
+    const localStorageUserSurname = prompt("Soyisim giriniz");
+    localStorage.setItem("Name", localStorageUserName);
+    localStorage.setItem("Surname", localStorageUserSurname);
 }
 //localStorageData();
 //////////////////////////////////////////////////////////////////////////////////////////////////////
